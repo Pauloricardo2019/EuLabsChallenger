59f70548684df5e1fa77f5edbd9e534e1cae55af
Pauloricardo2019 <paullin_santos13@hotmail.com>
2023-05-22 18:11:08 -0300

    :fix: remove database functions

--- adapter/repository/handler.go
+++ adapter/repository/handler.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"errors"
 	"gorm.io/gorm"
 )
 
@@ -18,47 +17,12 @@ func NewBaseRepository(db *gorm.DB) *BaseRepository {
 	}
 }
 
-func (b *BaseRepository) storeTransaction(ctx context.Context, connWithTransaction *gorm.DB) (context.Context, error) {
-	newCtx := context.WithValue(ctx, DbTransactionKey, connWithTransaction)
-	return newCtx, nil
-}
-
 func (b *BaseRepository) getTransaction(ctx context.Context) (*gorm.DB, error) {
 	connWithTransaction := ctx.Value(DbTransactionKey)
 	if connWithTransaction == nil {
 		return nil, nil
 	}
 	return connWithTransaction.(*gorm.DB), nil
-}
-
-// BeginTransaction - Generates a new DB transaction and insert it on the context.
-func (b *BaseRepository) BeginTransaction(ctx context.Context) (context.Context, error) {
-	connWithTransaction := b.db.Begin()
-	return b.storeTransaction(ctx, connWithTransaction)
-}
-
-func (b *BaseRepository) RollbackTransaction(ctx context.Context) error {
-	connWithTransaction, err := b.getTransaction(ctx)
-	if err != nil {
-		return err
-	}
-
-	if connWithTransaction == nil {
-		return errors.New("no transaction found on current context")
-	}
-	return connWithTransaction.Rollback().Error
-}
-
-func (b *BaseRepository) CommitTransaction(ctx context.Context) error {
-	connWithTransaction, err := b.getTransaction(ctx)
-	if err != nil {
-		return err
-	}
-
-	if connWithTransaction == nil {
-		return errors.New("no transaction found on current context")
-	}
-	return connWithTransaction.Commit().Error
 }
 
 func (b *BaseRepository) getConnection(ctx context.Context) (*gorm.DB, error) {
